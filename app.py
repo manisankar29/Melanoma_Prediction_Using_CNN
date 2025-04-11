6c5259a8cd02edfaff47943aa7e9dcbbdb0f9105
mani sankar pasala <manisankar249@gmail.com>
2025-04-11 15:51:00 +0530

    Updated app.py and render.yaml

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ def index():
 
     return render_template("index.html")
 
-if __name__ == '__main__':
-    import os
-    port = int(os.environ.get("PORT", 5000))  # Default to 5000 for local development
-    app.run(host='0.0.0.0', port=port, debug=True)
+#if __name__ == '__main__':
+   # import os
+   # port = int(os.environ.get("PORT", 10000))  
+   # app.run(host='0.0.0.0', port=port)
